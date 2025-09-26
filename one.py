d0cbdede8b285b207cdf6c46690b57e2c39e8c2a
jainamd05 <jainam.dudhediya123@gmail.com>
2025-09-26 21:01:46 +0530

    trying it again

--- one.py
+++ one.py
@@ -1,4 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris = sns.load_dataset('iris')
+print(iris.head())
+print(iris.describe())
+
 print("HELLO WORLD!")
 for i in range(3) :
     print(i)
 print("DONE!")
+
+print("I have leaarnt something new and tried implementing it...")
+for i in range(10) :
+    print(i*i)
+
+print("Implementation is good!")
+i = 0
+while True :
+    print("INFINITE LOOP!")
+    i += 1
+    if i == 25 :
+        break
